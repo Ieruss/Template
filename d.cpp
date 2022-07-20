fe01688ffc6e9ffba994f7619fff750ea4217629
Ieruss <82746962+Ieruss@users.noreply.github.com>
2022-07-20 12:34:47 +0600

    Add files via upload

---
+++ d.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#pragma GCC optimize ("unroll-loops,Ofast,O3")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define NFS ios_base::sync_with_stdio(0) , cin.tie(0) , cout.tie(0) ;
+#define file(s) if (fopen(s".in", "r")) freopen(s".in", "r", stdin), freopen(s".out", "w", stdout)
+template<typename T> using ordered_set = tree <T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*
+order_of_key (k) : Number of items strictly smaller than k .
+find_by_order(k) : K-th element in a set (counting from zero).
+*/
+
+typedef long long ll;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//template <class T> T rand(T l, T r) {
+//	return (uniform_int_distribution<T>(l, r))(rng);
+//}
+
+const int N = 3003;
+const int E = 27;
+
+void solve() {	
+	int n, k;
+	cin >> n >> k;
+	vec
+	for(int i = 1; i <= n; ++i) {
+		
+	}
+}
+
+signed main() {
+	NFS;
+	int test = 1;	
+	cin >> test;
+	while(test--){
+		solve();
+	}
+}
+
+
+
+
