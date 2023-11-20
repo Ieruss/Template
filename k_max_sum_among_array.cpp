da171e803a72a6bd6ec0e2188faea4e7caecdba3
Ibodullayev Ruslan <82746962+Ieruss@users.noreply.github.com>
2023-11-20 14:04:07 +0600

    Create k_max_sum_among_array.cpp

---
+++ k_max_sum_among_array.cpp
@@ -0,0 +1,33 @@
+struct Set{
+	int l;
+	ll sum = 0;
+	multiset<ll> S, T;
+	void add(int x) {
+		if (sz(S) < l) {
+			S.insert(x);
+			sum += x;
+		} else if (x > *S.begin()) {
+			S.insert(x);
+			sum += x;
+			T.insert(*S.begin());
+			sum -= *S.begin();
+			S.erase(S.begin());
+		} else {
+			T.insert(x);
+		}
+	}
+
+	void del(int x) {
+		if (sz(T) == 0 || x > *T.rbegin()) {
+			sum -= x;
+			S.erase(S.find(x));
+			if (!T.empty() && sz(S) < l) {
+				S.insert(*T.rbegin());
+				sum += *T.rbegin();
+				T.erase(--T.end());
+			}
+		} else {
+			T.erase(T.find(x));
+		}
+	}
+};
